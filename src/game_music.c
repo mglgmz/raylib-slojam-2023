da97959c096ef6de9a7e05e979eb1ddc7fe3e00e
Miguel Gomez Bistiancic <miguel.gomez.bistiancic@gmail.com>
2024-01-07 18:44:46 -0300

    Disable music

--- src/game_music.c
+++ src/game_music.c
@@ -2,44 +2,44 @@
 
 void InitGameMusic(void)
 {
-    InitAudioDevice();
-    LoadMusic("asteroids");
+    // InitAudioDevice();
+    // LoadMusic("asteroids");
 }
 
 void UpdateGameMusic(void)
 {
-    UpdateMusicStream(music);
+    //UpdateMusicStream(music);
 }
 
 void LoadMusic(char *musicName)
 {
-    if (musicName == currentMusicName)
-        return;
-    CloseMusic(currentMusicName);
-    currentMusicName = musicName;
-    music = LoadMusicStream(TextFormat("resources/sounds/music/%s.ogg", musicName));
-    music.looping = true;
+    // if (musicName == currentMusicName)
+    //     return;
+    // CloseMusic(currentMusicName);
+    // currentMusicName = musicName;
+    // music = LoadMusicStream(TextFormat("resources/sounds/music/%s.ogg", musicName));
+    // music.looping = true;
 }
 
 void PlayMusic(char *musicName)
 {
-    if (musicName != currentMusicName);
-        LoadMusic(musicName);
-    if(!IsMusicStreamPlaying(music))
-        PlayMusicStream(music);
+    // if (musicName != currentMusicName);
+    //     LoadMusic(musicName);
+    // if(!IsMusicStreamPlaying(music))
+    //     PlayMusicStream(music);
 }
 
 void PauseMusic(void)
 {
     paused = true;
-    PauseMusicStream(music);
+    //PauseMusicStream(music);
 }
 
 void ResumeMusic(void)
 {
     if (!paused)
         return;
-    ResumeMusicStream(music);
+    //ResumeMusicStream(music);
     paused = false;
 }
 
@@ -52,21 +52,21 @@ void CloseMusic(char *musicName)
 {
     if (musicName == currentMusicName)
         return;
-    UnloadMusicStream(music);
+    //UnloadMusicStream(music);
 }
 
 void UpdateMusicVolume(float newVolume)
 {
-    SetMusicVolume(music, newVolume);
+    //SetMusicVolume(music, newVolume);
 }
 
 void UpdateMusicPitch(float newPitch)
 {
-    SetMusicPitch(music, newPitch);
+    //SetMusicPitch(music, newPitch);
 }
 
 void ReleaseGameMusic(void)
 {
-    UnloadMusicStream(music);
+    //UnloadMusicStream(music);
     CloseAudioDevice();
 }
